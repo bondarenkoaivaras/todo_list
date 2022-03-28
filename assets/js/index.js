256723784ee0ed0be931d00e293aad76ffabcc55
AivarasBondarenko <aivarasbondarenko@gmail.com>
2022-03-28 19:42:31 +0300

    added remove all button

--- assets/js/index.js
+++ assets/js/index.js
@@ -32,11 +32,12 @@ class List {
     }
 
     addItems = () => { 
-       
+
             const input = document.querySelector('.user-input');
 
             if(input.value.length !== 0){
                 const listItem = this.createItem(input.value);
+                input.value = '';
                 const listBlock = document.querySelector('.list-block');
                 listBlock.appendChild(listItem);
             }else{
@@ -53,6 +54,7 @@ class List {
 
     createItem = (InputValue) => {
         const listItem = document.createElement('li');
+        listItem.classList = "list-item"
         listItem.innerHTML = `
         <div>
             <input type="checkbox" id="to_do-1" name="to_do-1" />
@@ -61,17 +63,47 @@ class List {
             <div class="edit">
             <i>edit</i>
             </div>
-            <div class="remov">
+            <div class="remove">
             <i>remove</i>
             </div>
         </div>
         `;
+
+        const remove = listItem.querySelector('.remove');
+        remove.addEventListener('click', (e) => {
+            e.target.parentElement.parentElement.parentElement.remove();
+        })
 
         return listItem;
 
     }
 
     createFooterBlock = () => {
+        const parentContainer = document.querySelector(this.containerClass);
+        const container = document.createElement('div');
+        const deleteAllButton =  document.createElement('button');
+        deleteAllButton.textContent = "Remove all"
+        deleteAllButton.className = 'remove-all';
+        container.className = 'list-footer__block';
+
+        deleteAllButton.addEventListener('click',this.removeAllCheckedItems);
+
+        container.appendChild(deleteAllButton);
+        parentContainer.appendChild(container)
+       
+    }
+
+    removeAllCheckedItems = () => {
+        const selectedItems = document.querySelectorAll('.list-block > li');
+        selectedItems.forEach(item => {
+            if(item.querySelector('input[type = "checkbox"]').checked){
+                item.remove();
+            }
+        })
+        this.updateProgressBar();
+    }
+
+    updateProgressBar = ()=> {
 
     }
 
